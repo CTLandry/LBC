14bbb0986c83c01471e0471c35dcdec233693165
Clinton Landry <clandry@appareo.com>
2020-05-10 22:03:33 -0700

    add swagger

--- LBC.Web.API/Startup.cs
+++ LBC.Web.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace LBC.Web.API
 {
@@ -26,6 +27,11 @@ namespace LBC.Web.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "LBC API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +43,13 @@ namespace LBC.Web.API
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "LBC API V1");
+            });
 
             app.UseRouting();
 
