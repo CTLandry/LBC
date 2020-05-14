6bfb5c570d3e32d9234c376c108949b8acd1cbc3
Clinton Landry <clandry@petsmart.com>
2020-05-13 20:50:43 -0700

    what is going on with the ignore unignore

---
+++ LBC/Services/SocialAuth/Exceptions/SocialAuthException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using LBC.Services.Base;
+
+namespace LBC.Services.SocialAuth.Exceptions
+{
+    public class SocialAuthException<T> : BaseServiceException<T>
+    {
+        public SocialAuthException()
+        {
+        }
+    }
+}
