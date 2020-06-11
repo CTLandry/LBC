252fb0ee2a55e9d8031b871c494d6ef71276260f
Clinton Landry <clandry@appareo.com>
2020-06-10 21:10:07 -0700

    update splash screen, add custom font, fix ios storyboard launcher, fix google auth button

--- LBC/AssemblyInfo.cs
+++ LBC/AssemblyInfo.cs
@@ -1,3 +1,6 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("FFF_Tusj.ttf", Alias = "Chalk")]
+[assembly: ExportFont("Pacifico.ttf", Alias = "Pacifico")]
