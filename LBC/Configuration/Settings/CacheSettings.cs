bd794381c3f7e9734b352d307eb306213a7ad3ed
Clinton Landry <clandry@appareo.com>
2020-06-20 19:51:57 -0700

    add files

---
+++ LBC/Configuration/Settings/CacheSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace LBC.Configuration.Settings
+{
+    public class CacheSettings
+    {
+        [JsonProperty("barrelid")]
+        public string barrelid { get; set; }
+    }
+}
---
+++ LBC/Models/User/User_Model.cs
@@ -0,0 +1,19 @@
+﻿
+using LBC.Models.Base.BaseModel;
+
+namespace LBC.Models.User
+{
+    public class User_Model : BaseModel
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+            
+        public User_Model()
+        {
+        }
+    }
+}
