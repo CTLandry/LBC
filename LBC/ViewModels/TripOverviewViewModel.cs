4480431e08056f7f2c87d9e67cb010eb48549c16
Clinton Landry <me@ctlandry.com>
2020-06-21 17:32:54 -0700

    Added landing page for post login

---
+++ LBC/ViewModels/TripOverviewViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using LBC.ViewModels.Base;
+
+namespace LBC.ViewModels
+{
+    public class TripOverviewViewModel : BaseViewModel
+    {
+        public TripOverviewViewModel()
+        {
+        }
+    }
+}
