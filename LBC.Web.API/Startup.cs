6d4b317bb2bd532696760296bc4b9fcc6a33d79c
Clinton Landry <me@ctlandry.com>
2020-06-24 08:07:45 -0700

    Add docker to web api

--- LBC.Web.API/Startup.cs
+++ LBC.Web.API/Startup.cs
@@ -27,6 +27,7 @@ namespace LBC.Web.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+          
 
             services.AddSwaggerGen(c =>
             {
